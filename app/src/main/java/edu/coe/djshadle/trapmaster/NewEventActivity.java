9b53d64f99800de5823b2a233472aab9f2c421cc
daltonshadle <daltonshadle@gmail.com>
2019-02-14 13:47:29 -0500

    DJS - Made New Event Activity landscape only
    Made New Event Activity landscape only and removed temporary ternary
    button code, will work on adding all 5 trap counters next time

--- app/src/main/java/edu/coe/djshadle/trapmaster/NewEventActivity.java
+++ app/src/main/java/edu/coe/djshadle/trapmaster/NewEventActivity.java
@@ -4,17 +4,28 @@ import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NewEventActivity extends AppCompatActivity implements OnTotalHitChange, OnStageChangeListener{
+public class NewEventActivity extends AppCompatActivity implements OnTotalHitChange {
+
+    //********************************** Variables and Constants ***********************************
+    // General Constants
+    private final boolean PORTRAIT_ORIENTATION = false; // Allow portrait orientation, landscape
+                                                        // is the default TODO: Support Portrait
+
+    // General Variables
+
+    // UI References
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            setContentView(R.layout.activity_new_event_landscape);
-        }
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT && PORTRAIT_ORIENTATION) {
             setContentView(R.layout.activity_new_event_portrait);
+        }
+        else {
+            // Landscape is the default orientation by AndroidManifest.xml
+            setContentView(R.layout.activity_new_event_landscape);
         }
 
         initViews();
@@ -23,18 +34,17 @@ public class NewEventActivity extends AppCompatActivity implements OnTotalHitCha
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
 
-        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT && PORTRAIT_ORIENTATION) {
             setContentView(R.layout.activity_new_event_portrait);
-        } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        }
+        else {
+            // Landscape is the default orientation by AndroidManifest.xml
             setContentView(R.layout.activity_new_event_landscape);
         }
 
     }
 
     private void initViews() {
-        TenaryButtonClass tempBtn = (TenaryButtonClass) findViewById(R.id.tbTempTer);
-        tempBtn.setStageChangeListener(this);
-
         TrapCounterButtonsClass tempCount= (TrapCounterButtonsClass) findViewById(R.id.tcbTempCounter);
         tempCount.setTotalHitChange(this);
     }
@@ -44,8 +54,4 @@ public class NewEventActivity extends AppCompatActivity implements OnTotalHitCha
 
     }
 
-    @Override
-    public void OnStageChange() {
-
-    }
 }
